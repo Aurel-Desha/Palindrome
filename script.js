84bd13614c19306028ca1d6c64b8dabe5d5cca7b
aurel-shael <deshyesguy2@gmail.com>
2024-02-16 21:46:27 +0100

    first

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const textInput = document.getElementById("text-input");
 const checkBtn = document.getElementById("check-btn");
+
+checkBtn.addEventListener("click", ()=>{
+  if(textInput.value === ""){
+    alert("Please input a value");
+  } else {
+    alert(textInput);
+  }
+})
